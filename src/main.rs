b857bf71e66722384b321732303b83552cf4c195
zico <zico@debian-BULLSEYE-live-builder-AMD64>
2023-10-26 18:02:20 -0600

    Added comments for SSI code

--- src/main.rs
+++ src/main.rs
@@ -118,36 +118,49 @@ fn handle_connection(mut stream: TcpStream) {
     stream.flush().unwrap();
 }
 
+// Find a subsequence in a sequence of u8's (Just String::find but for Vec<u8>)
 // https://stackoverflow.com/questions/35901547/how-can-i-find-a-subsequence-in-a-u8-slice
 fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
     haystack.windows(needle.len()).position(|window| window == needle)
 }
 
+// Read a file, but process it for server-side includes
 fn read_file_ssi(filename: &str) -> Vec<u8> {
-    print!("Got ssi file\n");
-    let mut file_string =fs::read(filename).unwrap();
-
+    // Define the tokens that we are searching for
     let include_token = b"<!-- #include ";
     let end_token = b" -->";
+    
+    // Read the file in
+    let mut file_string =fs::read(filename).unwrap();
 
+    // Try to find an include token
     let mut find_include = find_subsequence(&file_string,include_token);
 
+    // If we've got at least 1 SSI token, run through the loop to replace it with its content.
     while find_include != None
     {
-        print!("Found replaceable include\n");
+        // Get a substring of everything after the include token, and search that substring for the end token.
         let middle_end_str = &file_string[find_include.unwrap()..];
         let find_end = find_subsequence(middle_end_str,end_token);
+
+        // If we haven't found the end token, the SSI was done incorrectly. #NotMyProblem
         if find_end == None
         {
             return b"Unable to find end of include".to_vec();
         }
+
+        // Otherwise, lets make a clone of the existing content so that we can rebuild it and reference the previous content at the same time.
         let temp_file_string = file_string.clone();
-        let incuded_str = &temp_file_string[find_include.unwrap() + include_token.len()..find_include.unwrap()+find_end.unwrap()];
+
+        // Clear the existing string, and copy everything leading up to the include statement back onto it.
         file_string.clear();
         file_string.extend(&temp_file_string[..find_include.unwrap()]);
-
+        
+        // Pull the text from between the tokens and populate it into the ssi_filename we are looking for
+        let incuded_str = &temp_file_string[find_include.unwrap() + include_token.len()..find_include.unwrap()+find_end.unwrap()];
         let ssi_filename ="/home/zico/zerver/website/".to_owned() + &String::from_utf8(incuded_str.to_vec()).unwrap();
 
+        // Check if the ssi_filename is valid, and copy the content onto the file_string, or copy an error message in its place.
         if is_file_valid(Path::new(&ssi_filename))
         {
             file_string.extend(read_file_ssi(&ssi_filename));
@@ -156,13 +169,18 @@ fn read_file_ssi(filename: &str) -> Vec<u8> {
         {
             file_string.extend(b"Error: Unable to find ssi.");
         }
+
+        // Copy back everything that was after the end token
         file_string.extend(&temp_file_string[find_include.unwrap()+find_end.unwrap() + end_token.len()..]);
+
+        // Search for another include token, so that we can loop if we need to
         find_include = find_subsequence(&file_string,include_token);
     }
 
     return file_string;
 }
 
+// Read a file as-is
 fn read_file(filename: &str) -> Vec<u8> {
     return fs::read(filename).unwrap()
 }
